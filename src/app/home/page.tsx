2f1ad9b3205fb29ea0032f344ae35bb22f8df5e7
Joseph Haase <joseph.haase.dev@gmail.com>
2023-07-27 22:01:56 -0300

    updates

---
+++ src/app/home/page.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.scss';
+
+export default function HomeTemplate(): JSX.Element {
+  return <div>Home</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <main>Hello</main>;
+import HomeTemplate from './home/page';
+
+export default function Home(): JSX.Element {
+  return <HomeTemplate />;
 }
